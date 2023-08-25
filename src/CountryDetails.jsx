3385714d815311544383af5596fa23e0b7cfdb81
M.Waleed <waleeddar560@gmail.com>
2023-08-25 16:46:34 +0500

    2nd commit

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -13,7 +13,6 @@ export default function CountryDetails() {
       const url = `https://restcountries.com/v3.1/name/${countryName}`;
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data)
       return data;
     } catch (error) {
       throw new Error(error);
